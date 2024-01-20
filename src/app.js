40b75c9594ca815ca190965216d8cd6c9ab09867
nicoc <clementes.nicolas@gmail.com>
2024-01-20 16:31:25 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,41 +1,45 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors"; // 
+import cors from "cors";
 import productosRoutes from "./routes/productos.js";
 import carritoRoutes from "./routes/carrito.js";
 
-// Configuración de variables de entorno
 dotenv.config();
 
-// Inicialización de Express
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
-// Conexión a MongoDB
+// Conexión a MongoDB Atlas usando la variable de entorno
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Conexión establecida con MongoDB");
-})
-.catch((error) => {
-  console.error("Error al conectar con MongoDB:", error.message);
 });
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error de conexión a MongoDB: "));
+db.once("open", function () {
+  console.log("Conexión establecida con MongoDB Atlas");
+});
 
-// Configuración de rutas
 app.use(express.json());
 app.use("/formulario", express.static("public"));
 app.use("/api/productos", productosRoutes);
 app.use("/api/carrito", carritoRoutes);
 
-// Iniciar servidor
 try {
-  app.listen(PORT, () => {
-    console.log("Aplicación corriendo en puerto", PORT);
+  const server = app.listen(PORT, () => {
+    console.log("Aplicación corriendo en puerto", server.address().port);
+  });
+
+  server.on("error", (error) => {
+    console.error("Error en servidor:", error.message);
+  });
+
+  process.on("exit", () => {
+    console.log("Desconexión de la base de datos");
+    mongoose.disconnect();
   });
 } catch (error) {
   console.error(error);
