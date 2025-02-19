ebda08f7b22d2cc3b9ceec15896be4951caae924
nicoc <clementes.nicolas@gmail.com>
2025-02-19 10:24:53 -0300

    update app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from './config/database.js';
-import corsOptions from './config/cors.js';
 import productosRoutes from "./routes/productos.js";
 import carritoRoutes from "./routes/carrito.js";
 import authRoutes from "./routes/auth.js";
@@ -11,8 +10,33 @@ import pagoRoutes from "./routes/pago.js";
 dotenv.config();
 
 const app = express();
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGINS?.split(',') || [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://ecommerce-electronica-cs.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (process.env.ALLOWED_ORIGINS?.split(',').includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -42,6 +66,7 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Manejo de rutas no encontradas
 app.use((req, res) => {
   res.status(404).json({ 
     error: 'Ruta no encontrada',
